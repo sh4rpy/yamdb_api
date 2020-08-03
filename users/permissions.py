a8fff71c7751db95a03700aa2741298e04e6ee25
Никита Левашов <sh4rpy@ya.ru>
2020-08-03 12:47:06 +0300

    changed the name of the permission class in users/permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsAdminorMe(permissions.BasePermission):
+class IsAdminOrMe(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             if request.path == '/api/v1/users/me/':
